af8443f8baa27d13fbe541e043e5ccf6580e6c1e
ipzsapp <ipzsapp@gmail.com>
2020-01-27 11:24:56 +0100

    added compatibility android 10 update library, compile and target version migration to androidx

--- cieidsdk/src/main/java/it/ipzs/cieidsdk/ciekeystore/CieProvider.kt
+++ cieidsdk/src/main/java/it/ipzs/cieidsdk/ciekeystore/CieProvider.kt
@@ -5,11 +5,16 @@ import java.security.Provider
 
 internal class CieProvider : Provider(CieProvider::class.java.simpleName, 1.0, "Provider per cie") {
 
-    companion object{
+    companion object {
         const val PROVIDER = "CIE"
     }
+
     init {
         put("KeyStore.$PROVIDER", CieKeyStore::class.java.name)
-        put("Cipher.RSA/ECB/PKCS1Padding", Cipher::class.java.name)
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
+            put("Signature.NONEwithRSA", CieSignatureImpl.None::class.java.name)
+        } else {
+            put("Cipher.RSA/ECB/PKCS1Padding", Cipher::class.java.name)
+        }
     }
 }
