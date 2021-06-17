86686132ee0bffae49a4133594f2c558e101e388
ipzsapp <ipzsapp@gmail.com>
2021-06-17 15:30:27 +0200

    Removed certificate pinning

--- cieidsdk/src/main/java/it/ipzs/cieidsdk/network/Endpoints.kt
+++ cieidsdk/src/main/java/it/ipzs/cieidsdk/network/Endpoints.kt
@@ -4,7 +4,4 @@ internal object Endpoints {
 
     const val idp = "Authn/SSL/Login2"
 
-    const val BASE_URL_CERTIFICATE = "idserver.servizicie.interno.gov.it"
-    const val PIN_ROOT = "sha256/Vjs8r4z+80wjNcr1YKepWQboSIRi63WsWXhIMN+eWys="
-
 }
